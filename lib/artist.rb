2b6113ea6621d8ddde693b17315c486a0e100280
Don Ward <10is0420@gmail.com>
2019-08-07 17:21:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+attr_accessor :name, :songs
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end
+  
+  def add_song(name)
+    @songs << name
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def find_or_create_by_name
+    
+  end
+  
+  
+end
